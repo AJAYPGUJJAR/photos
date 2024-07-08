7af6685d6d91469d146445f81e98275fe311520b
ajaypgujjar <ajaypgujjar@yahoo.com>
2024-07-08 22:52:25 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { useEffect } from 'react';
-import { saveAs } from 'file-saver';
-import AmEx from './res/AmEx.PNG'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ImageAmEx from './main/ImageAmEx';
 
 export default function App() {
-   
-  const downloadImage = () => {
-    const imageBlob = new Blob([AmEx], { type: 'image/png' }); 
-    saveAs(imageBlob, 'AmEx.jpg');
-  };
-
-  useEffect(() => {
-    downloadImage();
-  }, []);
-
+  
   return (
     <div>
-      <h1>Downloaded Image - AmEx.PNG</h1>
+      <h1>Welcome To My Photos App</h1>
+      <Router>
+        <Routes>
+          <Route path="/AmEx.PNG" element={<ImageAmEx />} />
+        </Routes>
+      </Router>
     </div>
   )
+
 }
